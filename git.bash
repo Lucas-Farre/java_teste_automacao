c08e111fcbcc3c885d1ac420d386d3b248ddabce
Lucas Arcas Farré <150536194+Lucas-Farre@users.noreply.github.com>
2026-04-14 01:52:10 -0300

    Update git.bash

--- git.bash
+++ git.bash
@@ -1,7 +1,5 @@
 cd /c/Users/lucas/Desktop/files
 git init
 git add .
-git commit -m "feat: automação de testes de login com Selenium e Java"
-git branch -M main
-git remote add origin https://github.com/Lucas-Farre/selenium-saucedemo.git
+git commit 
 git push -u origin main
